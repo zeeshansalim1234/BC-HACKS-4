81c46a930082c030b54f380efb9d0a709a12a938
zeeshansalim1234 <zeeshan.chougle@ucalgary.ca>
2022-11-26 13:10:05 -0800

    imported torch

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from firebase_admin import credentials, initialize_app, firestore
 from flask import Flask, Blueprint, request, jsonify, render_template, redirect, url_for
 from flask_cors import CORS
+import torch
 
 app = Flask(__name__)
 CORS(app)
